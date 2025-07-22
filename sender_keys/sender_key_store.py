97ecb246963c3c422b9e3b08fd3fa1691d016430
UNSHD-X7 <u.shadow@seznam.cz>
2025-07-22 11:51:02 +0200

    Update sender_key_store.py

--- sender_keys/sender_key_store.py
+++ sender_keys/sender_key_store.py
@@ -1,11 +1,28 @@
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
+import json
+import base64
 
 class SenderKeyRecord:
-    def __init__(self, sender_key_id: int, sender_chain_key: bytes, sender_signing_pub: bytes, message_index: int):
+    def __init__(self, sender_key_id, sender_chain_key, sender_signing_pub, message_index=0):
         self.sender_key_id = sender_key_id
         self.sender_chain_key = sender_chain_key
-        self.sender_signing_pub = sender_signing_pub  # raw bytes representation
+        self.sender_signing_pub = sender_signing_pub
         self.message_index = message_index
 
-    def public_key(self):
-        return Ed25519PublicKey.from_public_bytes(self.sender_signing_pub)
+    def to_json(self):
+        return json.dumps({
+            "sender_key_id": self.sender_key_id,
+            "sender_chain_key": base64.b64encode(self.sender_chain_key).decode(),
+            "sender_signing_pub": base64.b64encode(self.sender_signing_pub).decode(),
+            "message_index": self.message_index
+        })
+
+    @staticmethod
+    def from_json(data):
+        obj = json.loads(data)
+        return SenderKeyRecord(
+            obj["sender_key_id"],
+            base64.b64decode(obj["sender_chain_key"]),
+            base64.b64decode(obj["sender_signing_pub"]),
+            obj["message_index"]
+        )
+
